584af48d380ccdc5e2051f9d116f7e693d149f3a
David Pick <pickdavid@gmail.com>
2011-11-22 18:45:38 -0600

    changed element to value

--- ruby/linked_list/linked_list_spec.rb
+++ ruby/linked_list/linked_list_spec.rb
@@ -1,46 +1,46 @@
 require 'rspec'
-require 'linkedlist'
+require 'linked_list'
 
 describe LinkedList do
   before do
     @list = LinkedList.new
   end
 
-  context 'adding elements' do
+  context 'adding values' do
     it 'has a nil head and tail for a new list' do
       @list.head.should be_nil
       @list.tail.should be_nil
     end
 
-    it 'inserts an element into an empty list' do
+    it 'inserts an value into an empty list' do
       @list.add('foo')
 
       @list.head.value.should == 'foo'
-      @list.head.next.element.should be_nil
+      @list.head.next.value.should be_nil
       @list.tail.value.should == 'foo'
     end
 
-    it 'inserts two elements into an empty list' do
+    it 'inserts two values into an empty list' do
       @list.add('foo')
       @list.add('bar')
 
-      @list.head.element.should == 'foo'
-      @list.head.next.element.should == 'bar'
-      @list.tail.element.should == 'bar'
+      @list.head.value.should == 'foo'
+      @list.head.next.value.should == 'bar'
+      @list.tail.value.should == 'bar'
     end
 
-    it 'inserts multiple elements into the list' do
+    it 'inserts multiple values into the list' do
       @list.add('foo')  
       @list.add('bar')  
       @list.add('example')  
 
-      @list.head.element.should == 'foo'
-      @list.head.next.element.should == 'bar'
-      @list.tail.element.should == 'example'
+      @list.head.value.should == 'foo'
+      @list.head.next.value.should == 'bar'
+      @list.tail.value.should == 'example'
     end
   end
 
-  context 'removing elements' do
+  context 'removing values' do
     before do
       @list.add('a')
       @list.add('b')
@@ -51,24 +51,24 @@ describe LinkedList do
     it 'removes the tail if no argument is passed to remove' do
       @list.remove
 
-      @list.tail.element.should == 'c'
+      @list.tail.value.should == 'c'
       @list.tail.next.should be_nil
     end
 
-    it 'removes an element in the middle of the list' do
+    it 'removes an value in the middle of the list' do
       @list.remove('b')
 
-      @list.head.next.element.should == 'c'
+      @list.head.next.value.should == 'c'
     end
 
     it 'removes the tail multiple times' do
       @list.remove
       @list.remove
 
-      @list.tail.element.should == 'b'
+      @list.tail.value.should == 'b'
     end
 
-    it 'removes multiple elements from the middle of the list' do
+    it 'removes multiple values from the middle of the list' do
       @list.remove('b')
       @list.remove('c')
 
@@ -78,11 +78,11 @@ describe LinkedList do
     it 'removes the head' do
       @list.remove(@list.head)
 
-      @list.head.element.should == 'b'
+      @list.head.value.should == 'b'
     end
   end
 
-  context 'checking if an element exists' do
+  context 'checking if an value exists' do
     before do
       @list.add('a')
       @list.add('b')
@@ -90,16 +90,16 @@ describe LinkedList do
       @list.add('d')
     end
 
-    it 'returns true if the element is in the list' do
+    it 'returns true if the value is in the list' do
       @list.exists?('b').should be_true
     end
 
-    it 'returns false if the element is not in the list' do
+    it 'returns false if the value is not in the list' do
       @list.exists?('f').should be_false
     end
   end
 
-  context 'finding the index of an element' do
+  context 'finding the index of an value' do
     before do
       @list.add('a')
       @list.add('b')
@@ -107,15 +107,15 @@ describe LinkedList do
       @list.add('d')
     end   
 
-    it 'returns 0 for the index of the head element' do
-      @list.index_of(@list.head.element).should == 0
+    it 'returns 0 for the index of the head value' do
+      @list.index_of(@list.head.value).should == 0
     end
 
-    it 'returns the index of an element if it is in the list' do
+    it 'returns the index of an value if it is in the list' do
       @list.index_of('c').should == 2
     end
     
-    it 'returns nil for the index if the element is not in the list' do
+    it 'returns nil for the index if the value is not in the list' do
       @list.index_of('f').should be_nil
     end
   end
